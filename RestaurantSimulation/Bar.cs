fef4764c8cad4c516e2480ab23f389607a3d4c96
medof <medofs2@gmail.com>
2016-11-18 13:50:54 +0100

    Project solution
    Created GUI, changed interface item names (button,label etc) to
    appropriate names. Added Table, MergedTable and Bar classes.

---
+++ RestaurantSimulation/Bar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSimulation
+{
+    class Bar
+    {
+        private int id;
+        private static int count;
+        public int size;
+        public bool available;
+
+        /// <summary>
+        /// Creates new Bar object
+        /// </summary>
+        /// <param name="size"></param>
+        public Bar(int size)
+        {
+            this.size = size;
+            this.id = count;
+            this.available = true;
+            count++;
+        }
+    }
+}
